2f6a15b09d4079cd670da61e23c6c43d8edb3713
Sey Emmanuel Ed-wilson <edseyjr@gmail.com>
2026-05-25 10:57:45 +0000

    fix github pages path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/shuttle-web/',  // must match your repo name exactly
+  base: '/shuttle_web/'
 })
